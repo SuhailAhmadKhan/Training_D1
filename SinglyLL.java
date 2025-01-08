ac97a6a1f31eb96d5edfd28e1942735e04758841
SuhailAhmadKhan <97402494+SuhailAhmadKhan@users.noreply.github.com>
2025-01-07 22:49:53 -0800

    Add files via upload

---
+++ SinglyLL.java
@@ -0,0 +1,17 @@
+
+    public void display() {    
+        //Node current will point to head    
+        Node current = head;    
+            
+        if(head == null) {    
+            System.out.println("List is empty");    
+            return;    
+        }    
+        System.out.println("Nodes of singly linked list: ");    
+        while(current != null) {    
+            //Prints each node by incrementing pointer    
+            System.out.print(current.data + " ");    
+            current = current.next;    
+        }    
+        System.out.println();    
+    }    
